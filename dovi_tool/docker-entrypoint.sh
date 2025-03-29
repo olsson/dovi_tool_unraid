a9554bf4364e1d236cf9b547458f9d92ffe0f1ac
Erik Olsson <erik@mrpeachum.se>
2025-03-29 22:43:51 +0100

    Revert

--- dovi_tool/docker-entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Set up DNS configuration
-echo "nameserver 8.8.8.8" > /etc/resolv.conf
-echo "nameserver 8.8.4.4" >> /etc/resolv.conf
-
-# Run the main entrypoint script
-exec /usr/local/bin/entrypoint.sh "$@" 
